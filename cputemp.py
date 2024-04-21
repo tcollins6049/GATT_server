8d79491b694c345a1248b42d55037435eacd58ad
Tyler Collins <collinstm@appstate.edu>
2024-04-21 18:06:23 -0500

    Added FileCharacteristic

--- cputemp.py
+++ cputemp.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python3
 
-"""Copyright (c) 2019, Douglas Otwell
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import dbus
 
 from advertisement import Advertisement
@@ -51,6 +30,29 @@ class ThermometerService(Service):
 
     def set_farenheit(self, farenheit):
         self.farenheit = farenheit
+
+
+class FileCharacteristic(Characteristic):
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index,
+            '00000005-710e-4a5b-8d75-3e5b444bc3cf', 
+            ['read', 'write'],  
+            service)
+        self.file_path = '/path/to/your/file' 
+
+    def ReadValue(self, options):
+        with open(self.file_path, 'r') as file:
+            data = file.read()
+        print('FileCharacteristic Read: {}'.format(data))
+        return [dbus.Byte(c) for c in data.encode()]
+
+    def WriteValue(self, value, options):
+        data = ''.join(chr(v) for v in value)
+        print('FileCharacteristic Write: {}'.format(data))
+        with open(self.file_path, 'w') as file:
+            file.write(data)
+
 
 class TempCharacteristic(Characteristic):
     TEMP_CHARACTERISTIC_UUID = "00000002-710e-4a5b-8d75-3e5b444bc3cf"
