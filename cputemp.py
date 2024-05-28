ac72a75cd28819468c2953acae0512af933fc196
Tyler Collins <collinstm@appstate.edu>
2024-05-28 18:22:59 -0500

    Fixing device names

--- cputemp.py
+++ cputemp.py
@@ -15,8 +15,8 @@ class ThermometerAdvertisement(Advertisement):
         self.add_service_uuid(ThermometerService.THERMOMETER_SVC_UUID)
         self.include_tx_power = True
         # Uncomment the line below to add a local name to the advertisement
-        self.add_local_name("")
-        print("local name not set")
+        # self.add_local_name("")
+        print("local name still not set")
 
 
 class ThermometerService(Service):
