529ba410bb2e0e66627fa2635ed8037bb4b8016a
Tyler Collins <collinstm@appstate.edu>
2024-05-29 22:52:05 -0500

    Adding cpu_temp characteristic

--- cputemp.py
+++ cputemp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import dbus, os, socket, glob
+import dbus, os, socket, glob, time
 from advertisement import Advertisement
 from service import Application, Service, Characteristic, Descriptor
 from gpiozero import CPUTemperature
