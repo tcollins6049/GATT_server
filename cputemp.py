9053702b9fa5008ff5f84721c35b8962d7e77eaf
Tyler Collins <collinstm@appstate.edu>
2024-06-13 14:35:31 -0500

    File transfers

--- cputemp.py
+++ cputemp.py
@@ -395,7 +395,8 @@ class FileTransferCharacteristic(Characteristic):
                 else:
                     self.offset += len(chunk)
 
-                return [dbus.Byte(b) for b in chunk]
+                # return [dbus.Byte(b) for b in chunk]
+                return list(chunk)
         except Exception as e:
             print(f"Error reading file: {e}")
             return []
