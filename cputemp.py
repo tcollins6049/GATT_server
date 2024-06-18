bf035058c6e8ab48cdb57349c3139c4302edbaae
Tyler Collins <collinstm@appstate.edu>
2024-06-18 13:41:35 -0500

    file transfers

--- cputemp.py
+++ cputemp.py
@@ -459,7 +459,7 @@ class FileTransferCharacteristic(Characteristic):
                 else:
                     self.offset += len(chunk)
 
-                print([dbus.Byte(b) for b in chunk])
+                print(chunk)
                 return [dbus.Byte(b) for b in chunk]
                 # return list(chunk)
         except Exception as e:
