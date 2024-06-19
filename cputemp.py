0c1f746ca5da15121fcf9ba294316411cf895007
Tyler Collins <collinstm@appstate.edu>
2024-06-19 14:25:13 -0500

    file transfers

--- cputemp.py
+++ cputemp.py
@@ -64,6 +64,7 @@ class ThermometerService(Service):
 
         # Adding a characteristic for pulling a file
         file_transfer_characteristic = (FileTransferCharacteristic(self, '00000011-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/video/2024-06-13/rpi4-60@2024-06-13@15-10-00.h264'))
+        self.add_characteristic(file_transfer_characteristic)
 
         # Adding file-related variable change characteristics for video
         self.add_characteristic(FileCharacteristic(self, '00000012-710e-4a5b-8d75-3e5b444bc3cf', 'video','capture_window_start_time'))
@@ -442,39 +443,6 @@ class FileTransferCharacteristic(Characteristic):
         self.file_path = file_path
         self.offset = 0
         print(f"FileTransferCharacteristic initialized with UUID: {uuid}")
-
-    
-    def get_most_recent_file(self, base_path):
-        print("Getting most recent file")
-        # List all directories in the base path
-        entries = os.listdir(base_path)
-        
-        # Filter out possible non directory entries or directories which dont match the data format
-        date_dirs = []
-        for entry in entries:
-            entry_path = os.path.join(base_path, entry)
-            if os.path.isdir(entry_path):
-                try:
-                    # Try to parse the directory name as a date
-                    date = datetime.strptime(entry, "%Y-%m-%d")
-                    date_dirs.append((entry, date))
-                except ValueError:
-                    # Skip directories that don't match the date format
-                    pass
-        if not date_dirs:
-            return None
-
-        # Find most recent date
-        most_recent_dir = max(date_dirs, key=lambda x: x[1])[0]
-        full_path = os.path.join(base_path, most_recent_dir)
-
-        # List files in this directory
-        files = os.listdir(full_path)
-        if (len(files) != 1):
-            raise ValueError(f"Expected exactly one file in directory {full_path}, found {len(files)}")
-        
-        # Get full path of the file
-        return full_path + '/' + files[0]
         
     
     def extract_frame(self, video_file, frame_number, output_file):
@@ -530,11 +498,8 @@ class FileTransferCharacteristic(Characteristic):
             # mtu = options.get('mtu', 512) - 3  # subtract 3 bytes for ATT header
             mtu = 512
 
-            # base_path = self.get_most_recent_file(self.file_path)
-            # print("BASE_PATH: ", base_path)
             image_path = self.extract_frame(self.file_path, 100, '/home/tcollins6049/GATT_server/output_frame.jpg')
 
-            
             with open(image_path, 'rb') as file:
                 file.seek(self.offset)
                 chunk = file.read(mtu)
@@ -547,7 +512,6 @@ class FileTransferCharacteristic(Characteristic):
 
                 self.delete_file(image_path)
                 return [dbus.Byte(b) for b in chunk]
-            
                 
         except Exception as e:
             print(f"Error reading file: {e}")
