f732cf5554963caf4e86caf66baf6db9a1db5104
Tyler Collins <collinstm@appstate.edu>
2024-06-20 10:30:04 -0500

    file transfers

--- cputemp.py
+++ cputemp.py
@@ -443,6 +443,41 @@ class FileTransferCharacteristic(Characteristic):
         self.file_path = file_path
         self.offset = 0
         print(f"FileTransferCharacteristic initialized with UUID: {uuid}")
+    
+
+    def get_most_recent_file(self, base_path):
+        print("Getting most recent file")
+        # List all directories in the base path
+        entries = os.listdir(base_path)
+        
+        # Filter out possible non directory entries or directories which dont match the data format
+        date_dirs = []
+        for entry in entries:
+            entry_path = os.path.join(base_path, entry)
+            if os.path.isdir(entry_path):
+                try:
+                    # Try to parse the directory name as a date
+                    date = datetime.strptime(entry, "%Y-%m-%d")
+                    date_dirs.append((entry, date))
+                except ValueError:
+                    # Skip directories that don't match the date format
+                    pass
+        if not date_dirs:
+            return None
+
+        # Find most recent date
+        most_recent_dir = max(date_dirs, key=lambda x: x[1])[0]
+        full_path = os.path.join(base_path, most_recent_dir)
+
+        print("Directories in video folder: ", full_path)
+
+        # List files in this directory
+        # files = os.listdir(full_path)
+        # if (len(files) != 1):
+        #     raise ValueError(f"Expected exactly one file in directory {full_path}, found {len(files)}")
+        
+        # Get full path of the file
+        # return full_path + '/' + files[0]
         
     
     def extract_frame(self, video_file, frame_number, output_file):
@@ -495,6 +530,11 @@ class FileTransferCharacteristic(Characteristic):
     
     def ReadValue(self, options):
         try:
+            temp_base_path = '/home/bee/appmais/bee_tmp/video/'
+            self.get_most_recent_file(temp_base_path)
+
+
+
             # mtu = options.get('mtu', 512) - 3  # subtract 3 bytes for ATT header
             mtu = 512
 
