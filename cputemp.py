275ef1232a826bf2a759da511685bfbab4720d33
Tyler Collins <collinstm@appstate.edu>
2024-06-21 18:30:07 -0500

    GATT restructuring

--- cputemp.py
+++ cputemp.py
@@ -68,9 +68,9 @@ class ThermometerService(Service):
         self.add_characteristic(video_file_transfer_characteristic)
         self.add_characteristic(ResetOffsetCharacteristic(self, '00000022-710e-4a5b-8d75-3e5b444bc3cf', video_file_transfer_characteristic))
 
-        audio_file_transfer_characteristic = (FileTransferCharacteristic(self, '00000025-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/audio/2024-05-29/rpi4-60@2024-05-29@14-20-00.wav', 'audio'))
-        self.add_characteristic(audio_file_transfer_characteristic)
-        self.add_characteristic(ResetOffsetCharacteristic(self, '00000024-710e-4a5b-8d75-3e5b444bc3cf', audio_file_transfer_characteristic))
+        #audio_file_transfer_characteristic = (FileTransferCharacteristic(self, '00000025-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/audio/2024-05-29/rpi4-60@2024-05-29@14-20-00.wav', 'audio'))
+        #self.add_characteristic(audio_file_transfer_characteristic)
+        #self.add_characteristic(ResetOffsetCharacteristic(self, '00000024-710e-4a5b-8d75-3e5b444bc3cf', audio_file_transfer_characteristic))
 
         # --------------- Tab 3: Sensor data -- Characteristics ----------------- #
         # Adding a characterisitc for cpu file data
