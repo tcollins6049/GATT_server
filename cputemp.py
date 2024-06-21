52c227e7aae9c5f649207a9c72f66fa320137503
Tyler Collins <collinstm@appstate.edu>
2024-06-20 22:42:31 -0500

    GATT restructuring

--- cputemp.py
+++ cputemp.py
@@ -48,33 +48,33 @@ class ThermometerService(Service):
         # Initialize the base Service class with the service UUID
         Service.__init__(self, index, self.THERMOMETER_SVC_UUID, True)
 
-        # Add characteristics to the service
-        self.add_characteristic(TempCharacteristic(self))
-        self.add_characteristic(UnitCharacteristic(self))
-        
+        # -------------- Tab 1: Variables -- Characteristics ------------------- #
         # Adding file-related variable change characteristics
         self.add_characteristic(FileCharacteristic(self, '00000005-710e-4a5b-8d75-3e5b444bc3cf', 'global','capture_window_start_time'))
         self.add_characteristic(FileCharacteristic(self, '00000006-710e-4a5b-8d75-3e5b444bc3cf', 'global', 'capture_window_end_time'))
         self.add_characteristic(FileCharacteristic(self, '00000007-710e-4a5b-8d75-3e5b444bc3cf', 'global', 'capture_duration_seconds'))
         self.add_characteristic(FileCharacteristic(self, '00000008-710e-4a5b-8d75-3e5b444bc3cf', 'global', 'capture_interval_seconds'))
-
-        # Adding a characteristic for file information (e.g., file size)
-        self.add_characteristic(FileInfoCharacteristic(self, '00000009-710e-4a5b-8d75-3e5b444bc3cf'));
-    
-        # Adding a characterisitc for cpu file data
-        self.add_characteristic(CPUFileReadCharacteristic(self, '00000010-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/cpu/'))
-        self.add_characteristic(CPUFileReadCharacteristic(self, '00000024-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/temp/'))
-
-        # Adding a characteristic for pulling a file
-        file_transfer_characteristic = (FileTransferCharacteristic(self, '00000011-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/video/'))
-        self.add_characteristic(file_transfer_characteristic)
-
         # Adding file-related variable change characteristics for video
         self.add_characteristic(FileCharacteristic(self, '00000012-710e-4a5b-8d75-3e5b444bc3cf', 'video','capture_window_start_time'))
         self.add_characteristic(FileCharacteristic(self, '00000013-710e-4a5b-8d75-3e5b444bc3cf', 'video', 'capture_window_end_time'))
         self.add_characteristic(FileCharacteristic(self, '00000014-710e-4a5b-8d75-3e5b444bc3cf', 'video', 'capture_duration_seconds'))
         self.add_characteristic(FileCharacteristic(self, '00000015-710e-4a5b-8d75-3e5b444bc3cf', 'video', 'capture_interval_seconds'))
 
+        # ------------- Tab 2: Audio + Video -- Characteristics ------------------ #
+        # Adding a characteristic for file information (e.g., file size)
+        self.add_characteristic(FileInfoCharacteristic(self, '00000009-710e-4a5b-8d75-3e5b444bc3cf'));
+        # Adding a characteristic for pulling a file
+        file_transfer_characteristic = (FileTransferCharacteristic(self, '00000011-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/video/'))
+        self.add_characteristic(file_transfer_characteristic)
+        # Adding characteristic to reset offset when getting file
+        self.add_characteristic(ResetOffsetCharacteristic(self, '00000022-710e-4a5b-8d75-3e5b444bc3cf', file_transfer_characteristic))
+
+        # --------------- Tab 3: Sensor data -- Characteristics ----------------- #
+        # Adding a characterisitc for cpu file data
+        self.add_characteristic(CPUFileReadCharacteristic(self, '00000010-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/cpu/'))
+        self.add_characteristic(CPUFileReadCharacteristic(self, '00000024-710e-4a5b-8d75-3e5b444bc3cf', '/home/bee/appmais/bee_tmp/temp/'))
+
+        # --------------- Tab 4: Sensor State Management -- Characteristics ----------------- #
         # Adding characteristics for enabling and disabling sensors
         self.add_characteristic(SensorStateCharacteristic(self, '00000016-710e-4a5b-8d75-3e5b444bc3cf', 'audio'))
         self.add_characteristic(SensorStateCharacteristic(self, '00000017-710e-4a5b-8d75-3e5b444bc3cf', 'video'))
@@ -83,11 +83,14 @@ class ThermometerService(Service):
         self.add_characteristic(SensorStateCharacteristic(self, '00000020-710e-4a5b-8d75-3e5b444bc3cf', 'scale'))
         self.add_characteristic(SensorStateCharacteristic(self, '00000021-710e-4a5b-8d75-3e5b444bc3cf', 'cpu'))
 
-        # Adding characteristic to reset offset when getting file
-        self.add_characteristic(ResetOffsetCharacteristic(self, '00000022-710e-4a5b-8d75-3e5b444bc3cf', file_transfer_characteristic))
-
+        # --------------- Tab 5: Commands -- Characteristics ---------------- #
         # Adding the new command characteristic
         self.add_characteristic(CommandCharacteristic(self))
+
+        # -------------- Sensor Readings -- Characteristics ---------------- #
+        # Add characteristics to the service
+        self.add_characteristic(TempCharacteristic(self))
+        self.add_characteristic(UnitCharacteristic(self))
 
 
     # Method to check if the temperature unit is Fahrenheit
