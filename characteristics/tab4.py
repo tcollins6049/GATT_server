803a23057b6090208e4106c28602507c35f8bbf3
Tyler Collins <collinstm@appstate.edu>
2024-06-21 21:06:40 -0500

    file transfers

--- characteristics/tab4.py
+++ characteristics/tab4.py
@@ -106,22 +106,22 @@ class SensorStateCharacteristic(Characteristic):
 
             values = []
 
-            # Check if the section exists and auto_start is explicitly set to False
-            if self.section_name in config and self.variable_name in config[self.section_name]:
-                value = config[self.section_name][self.variable_name]
-                if value.lower() == 'true':
-                    values.append(f"{self.section_name}: True")
-                elif value.lower() == 'false':
-                    # Don't append anything if auto_start is explicitly set to False
-                    pass
-            else:
-                # If auto_start variable is not found in the section, consider it True
-                values.append(f"{self.section_name}: True")
+            # Iterate through all sections in the configuration
+            for section in config.sections():
+                # Check if the auto_start variable exists in the section
+                if self.variable_name in config[section]:
+                    value = config[section][self.variable_name]
+                    if value.lower() == 'true':
+                        values.append(f"{section}: True")
+                    elif value.lower() == 'false':
+                        values.append(f"{section}: False")
+                else:
+                    # If auto_start variable is not found in the section, consider it True
+                    values.append(f"{section}: True")
 
             if values:
                 captured_data = '\n'.join(values)
-                
-                print(f"FileCharacteristic Read: {captured_data}")
+                print(f"FileCharacteristic Read:\n{captured_data}")
                 return [dbus.Byte(c) for c in captured_data.encode()]
             else:
                 print(f"Variable {self.variable_name} not found in any applicable section")
@@ -167,6 +167,3 @@ class SensorStateCharacteristic(Characteristic):
 
         except Exception as e:
             print(f"Error Writing File: {e}")
-
-    
-
