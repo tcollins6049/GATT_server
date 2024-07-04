72b9bd11672d29b34916d66dbcc5b79be30b3236
Tyler Collins <collinstm@appstate.edu>
2024-07-04 13:18:20 -0500

    Password Verification

--- cputemp.py
+++ cputemp.py
@@ -120,15 +120,16 @@ class PasswordVerificationCharacteristic(Characteristic):
     def __init__(self, service, uuid, password_file):
         Characteristic.__init__(
             self, uuid,
-            ['write'], service)
+            ['write', 'read'], service)
         self.password_file = password_file
+        self.is_correct_password = False
         print(f"PasswordVerificationCharacteristic initialized with UUID: {uuid}")
 
 
     def ReadValue(self, options):
         return [dbus.Byte(1) if self.is_correct_password else dbus.Byte(0)]
     
-    
+
     def WriteValue(self, value, options):
         input_password = bytes(value).decode()
         with open(self.password_file, 'r') as file:
@@ -136,10 +137,12 @@ class PasswordVerificationCharacteristic(Characteristic):
         
         if input_password == stored_password:
             print("Password is correct")
-            return [dbus.Byte(1)]
+            self.is_correct_password = True
+            # return [dbus.Byte(1)]
         else:
             print("Password is incorrect")
-            return [dbus.Byte(0)]
+            self.is_correct_password = False
+            # return [dbus.Byte(0)]
 
 
 
