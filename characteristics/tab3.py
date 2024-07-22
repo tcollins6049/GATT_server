eff249a5650a36989c8b06d619b772902c2bfc3b
Tyler Collins <collinstm@appstate.edu>
2024-07-21 20:37:49 -0500

    Line by line reading ch

--- characteristics/tab3.py
+++ characteristics/tab3.py
@@ -204,24 +204,36 @@ class CPUReadLineByLineCharacteristic(Characteristic):
     
     def get_most_recent_file(self, base_path):
         print("Getting most recent file")
+        # List all directories in the base path
         entries = os.listdir(base_path)
+        
+        # Filter out possible non directory entries or directories which dont match the data format
         date_dirs = []
         for entry in entries:
             entry_path = os.path.join(base_path, entry)
             if os.path.isdir(entry_path):
                 try:
+                    # Try to parse the directory name as a date
                     date = datetime.strptime(entry, "%Y-%m-%d")
                     date_dirs.append((entry, date))
                 except ValueError:
+                    # Skip directories that don't match the date format
                     pass
         if not date_dirs:
             return None
+
+        # Find most recent date
         most_recent_dir = max(date_dirs, key=lambda x: x[1])[0]
         full_path = os.path.join(base_path, most_recent_dir)
+
+        # List files in this directory
         files = os.listdir(full_path)
-        if len(files) != 1:
+        if (len(files) != 1):
             raise ValueError(f"Expected exactly one file in directory {full_path}, found {len(files)}")
-        return os.path.join(full_path, files[0])
+        
+        # Get full path of the file
+        return full_path + '/' + files[0]
+
 
     def ReadValue(self, options):
         print("ReadValue called")
