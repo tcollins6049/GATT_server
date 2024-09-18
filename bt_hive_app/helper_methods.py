d782cbafaf305306e16798002596b483a24b8030
Tyler Collins <collinstm@appstate.edu>
2024-09-18 13:33:51 -0500

    Fixing sensor file transfer

--- bt_hive_app/helper_methods.py
+++ bt_hive_app/helper_methods.py
@@ -8,42 +8,42 @@ import matplotlib.pyplot as plt
 
 def get_most_recent_sensor_file(base_path):
     print("Getting most recent file")
-    try:
-        # List all directories in the base path
-        entries = os.listdir(base_path)
-        print(f"Entries: {entries}")
+    #try:
+    # List all directories in the base path
+    entries = os.listdir(base_path)
+    print(f"Entries: {entries}")
             
-        # Filter out possible non directory entries or directories which dont match the data format
-        date_dirs = []
-        for entry in entries:
-            entry_path = os.path.join(base_path, entry)
-            if os.path.isdir(entry_path):
-                try:
-                    # Try to parse the directory name as a date
-                    date = datetime.strptime(entry, "%Y-%m-%d")
-                    date_dirs.append((entry, date))
-                except ValueError:
-                    # Skip directories that don't match the date format
-                    pass
-        if not date_dirs:
-            print("Not date_dirs")
-            return None
+    # Filter out possible non directory entries or directories which dont match the data format
+    date_dirs = []
+    for entry in entries:
+        entry_path = os.path.join(base_path, entry)
+        if os.path.isdir(entry_path):
+            try:
+                # Try to parse the directory name as a date
+                date = datetime.strptime(entry, "%Y-%m-%d")
+                date_dirs.append((entry, date))
+            except ValueError:
+                # Skip directories that don't match the date format
+                pass
+    if not date_dirs:
+        print("Not date_dirs")
+        return None
 
-        # Find most recent date
-        most_recent_dir = max(date_dirs, key=lambda x: x[1])[0]
-        print(f"Most_recent_dir: {most_recent_dir}")
-        full_path = os.path.join(base_path, most_recent_dir)
-        print(f"Full_path: {full_path}")
+    # Find most recent date
+    most_recent_dir = max(date_dirs, key=lambda x: x[1])[0]
+    print(f"Most_recent_dir: {most_recent_dir}")
+    full_path = os.path.join(base_path, most_recent_dir)
+    print(f"Full_path: {full_path}")
 
-        # List files in this directory
-        files = os.listdir(full_path)
-        if (len(files) != 1):
-            raise ValueError(f"Expected exactly one file in directory {full_path}, found {len(files)}")
-            
-        # Get full path of the file
-        return full_path + '/' + files[0]
-    except:
-        print("Failure within get_most_recent_sensor_file()")
+    # List files in this directory
+    files = os.listdir(full_path)
+    if (len(files) != 1):
+        raise ValueError(f"Expected exactly one file in directory {full_path}, found {len(files)}")
+        
+    # Get full path of the file
+    return full_path + '/' + files[0]
+    #except:
+    #    print("Failure within get_most_recent_sensor_file()")
 
 
 def create_waveform_file(audio_file):
