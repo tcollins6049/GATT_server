80a7c003c95d037a063078a29f861ee5556477dc
Tyler Collins <collinstm@appstate.edu>
2024-10-16 12:36:50 -0500

    Fixing take picture

--- bt_hive_app/characteristics/audio_video.py
+++ bt_hive_app/characteristics/audio_video.py
@@ -174,7 +174,7 @@ class FileTransferCharacteristic(Characteristic):
         elif self.file_type == 'audio':
             return self.readWaveformFile()
         elif self.file_type == 'other':
-            self.capturePicture()
+            # self.capturePicture()
             result = self.ReadStaticFile()
             # help.delete_file(self.image_path)
             return result
@@ -208,7 +208,6 @@ class FileTransferCharacteristic(Characteristic):
 
         """
         try:
-            # self.capturePicture()
             mtu = 512
             # self.image_path = self.file_path
             
