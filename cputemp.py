9d552cc164fe4a1457e1fdd21b4c95db85cffe0a
Tyler Collins <collinstm@appstate.edu>
2024-10-21 13:23:40 -0500

    Fixing imports

--- cputemp.py
+++ cputemp.py
@@ -8,7 +8,7 @@ import dbus
 # import cv2
 # from gpiozero import CPUTemperature
 
-from advertisement import Advertisement
+# from advertisement import Advertisement
 from service import Application, Service, Characteristic, Descriptor
 
 # Imports for services and advertisments
