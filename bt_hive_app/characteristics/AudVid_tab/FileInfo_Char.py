eb1865e0c018a853220ea8500110f2e9222fa322
Tyler Collins <collinstm@appstate.edu>
2024-10-29 13:01:13 -0500

    Restructuring code

--- bt_hive_app/characteristics/AudVid_tab/FileInfo_Char.py
+++ bt_hive_app/characteristics/AudVid_tab/FileInfo_Char.py
@@ -6,20 +6,27 @@ from pydub import AudioSegment
 
 class FileInfoCharacteristic(Characteristic):
     """
-    This class holds the charactersitics code for pulling info from audio and video files.
-    Pulls data such as file size and RMS level.
+    The following characteristic pulls data, such as file size, from audio and video files.
 
+    Attributes:
+        service (): Service containing this characteristic
+        uuid (str): uuid for the characteristic 
+        file_path (str): Path of file data is being pulled from
+        file_type (str): Either 'audio' or 'video' 
+    
+    Methods:
+        ReadValue(): Oversees process of reading file data
+        calculate_rms_and_check_silence(audio_segment): Calculates RMS level of audio segment
     """
     def __init__(self, service, uuid, file_path, file_type):
         """
-        init function
+        Initialize the class
 
         Args:
-            service: Service this characteristic is under
-            uuid (str): uuid for this characteristic
-            file_path (str): Path of the file we are reading info from
+            service: Service containing this characteristic
+            uuid (str): uuid for the characteristic
+            file_path (str): Path of file data is being pulled from
             file_type (str): Either 'audio' or 'video'.
-        
         """
         super().__init__(
             uuid,
@@ -29,26 +36,25 @@ class FileInfoCharacteristic(Characteristic):
         self.file_type = file_type
 
 
-    def ReadValue(self, options):
+    def ReadValue(self):
         """
         Function responsible for reading information from file at file_path
 
         Args:
             options: 
-
         """
         temp_file_path = self.file_path
 
+        # Get most recent sensor file
         if self.file_type == 'audio':
             temp_file_path = help.get_most_recent_audio_file(temp_file_path)
         elif self.file_type == 'video':
             temp_file_path = help.get_most_recent_video_file(temp_file_path)
 
-        
-        print("FILE PATH: ", temp_file_path)
-
+        # Get file size
         file_size_wav = os.path.getsize(temp_file_path)
 
+        # Convert .wav file to .mp3
         if temp_file_path.endswith('.wav'):
             audio = AudioSegment.from_wav(temp_file_path)
             mp3_file_name = os.path.basename(temp_file_path).replace('.wav', '.mp3')
@@ -57,12 +63,14 @@ class FileInfoCharacteristic(Characteristic):
             file_size_mp3 = os.path.getsize(mp3_file_path)
             os.remove(mp3_file_path)
 
+            # Calculate RMS level
             rms_level, silence_detected = self.calculate_rms_and_check_silence(audio)
         else:
             file_size_mp3 = 0
             rms_level = 0
             silence_detected = False
 
+        # Return file data
         file_info = (f"{temp_file_path}, File Size: {file_size_wav} bytes, File Size: {file_size_mp3} bytes, "
                      f"RMS Level: {rms_level}, Silence Detected: {'Yes' if silence_detected else 'No'}")
         print('FileInfoCharacteristic Read: {}'.format(file_info))
